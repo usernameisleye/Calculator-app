ec4b94a74a13313b34acd5dd3c153dea12ae4710
Adesida Leye <adeleyeadesida@gmail.com>
2023-02-03 19:53:59 +0100

    updated js file

--- js/script.js
+++ js/script.js
@@ -24,6 +24,69 @@ themeBtns.forEach(btn => {
     }
 })
 
+allButtons.forEach(button => {
+    button.addEventListener('click', removeBottom);
+
+    function removeBottom(e){
+        button.style.borderBottom = 'none';
+
+        setTimeout(() =>{
+                if(themeLink.href == 'http://127.0.0.1:5500/styles/themeOne.css'){
+                    if(button.classList.contains('number')){
+                        button.style.borderBottom = '3px solid hsl(28, 16%, 65%)';
+                    }
+                    else if(button.classList.contains('operation')){
+                        button.style.borderBottom = '3px solid hsl(28, 16%, 65%)';
+                    }
+                    else if(button.classList.contains('del')){
+                        button.style.borderBottom = '3px solid hsl(224, 28%, 35%)';
+                    }
+                    else if(button.classList.contains('reset')){
+                        button.style.borderBottom = '3px solid hsl(224, 28%, 35%)';
+                    }
+                    else{
+                        button.style.borderBottom = '3px solid hsl(6, 70%, 34%)';
+                    }
+                }
+                else if(themeLink.href == 'http://127.0.0.1:5500/styles/themeTwo.css'){
+                        if(button.classList.contains('number')){
+                            button.style.borderBottom = '3px solid hsl(28, 16%, 65%)';
+                        }
+                        else if(button.classList.contains('operation')){
+                            button.style.borderBottom = '3px solid hsl(28, 16%, 65%)';
+                        }
+                        else if(button.classList.contains('del')){
+                            button.style.borderBottom = '3px solid hsl(185, 58%, 25%)';
+                        }
+                        else if(button.classList.contains('reset')){
+                            button.style.borderBottom = '3px solid hsl(185, 58%, 25%)';
+                        }
+                        else{
+                            button.style.borderBottom = '3px solid hsl(25, 99%, 27%)';
+                        }
+                    }
+                else if(themeLink.href == 'http://127.0.0.1:5500/styles/themeThree.css'){
+                        if(button.classList.contains('number')){
+                            button.style.borderBottom = '3px solid hsl(290, 70%, 36%)';
+                        }
+                        else if(button.classList.contains('operation')){
+                            button.style.borderBottom = '3px solid hsl(290, 70%, 36%)';
+                        }
+                        else if(button.classList.contains('del')){
+                            button.style.borderBottom = '3px solid hsl(285, 91%, 52%)';
+                        }
+                        else if(button.classList.contains('reset')){
+                            button.style.borderBottom = '3px solid hsl(285, 91%, 52%)';
+                        }
+                        else{
+                            button.style.borderBottom = '3px solid hsl(177, 92%, 70%)';
+                        }
+                    }
+        }, 100)
+
+    }
+})
+
 
 //Main Calculator functions
 numberBtns.forEach(button =>{
